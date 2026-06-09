64b9e29c5463720fb2ea402e8be1008cff75fde4
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-06-09 13:24:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -22,8 +22,8 @@ provider "vault" {
     path = "auth/approle/login"
 
     parameters = {
-      role_id   = "967392ce-1878-c958-b251-78828d7ca55a"
-      secret_id = "a857182b-1cf0-d200-04a5-e38b34c3e774"
+      role_id   = " add role id"
+      secret_id = " add secret id"
     }
   }
 }
